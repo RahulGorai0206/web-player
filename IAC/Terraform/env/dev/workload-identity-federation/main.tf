0ced463e2fcf1643c363499f999f3620509f799f
Rahul Gorai <rahulgorai0206@gmail.com>
2026-02-03 21:23:38 +0530

    fixed pool ID

--- IAC/Terraform/env/dev/workload-identity-federation/main.tf
+++ IAC/Terraform/env/dev/workload-identity-federation/main.tf
@@ -21,14 +21,14 @@ resource "google_iam_workload_identity_pool_provider" "github_provider" {
     "google.subject"             = "assertion.sub"
     "attribute.actor"            = "assertion.actor"
     "attribute.repository"       = "assertion.repository"
-    "attribute.repository_owner" = "assertion.repository_owner"
+    # "attribute.repository_owner" = "assertion.repository_owner"
   }
 
-  attribute_condition = "assertion.repository_owner == '${var.github_owner}'"
+  attribute_condition = "assertion.repository == '${var.github_owner}/web-player'"
 }
 
 resource "google_service_account_iam_member" "impersonate_binding" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository_owner/${var.github_owner}"
+  member             = "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_owner}/web-player"
 }
